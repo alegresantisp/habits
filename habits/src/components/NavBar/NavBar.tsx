d8795ad112a33bb6cedbcc9b8660493930f7af36
SantiagoAlegre <alegresantisp@gmail.com>
2024-09-26 12:30:45 -0300

    changes

--- habits/src/components/NavBar/NavBar.tsx
+++ habits/src/components/NavBar/NavBar.tsx
@@ -84,7 +84,7 @@ const Navbar = () => {
       </div>
       <div className={`sm:hidden ${isOpen ? 'block' : 'hidden'}`}>
         <div className="px-2 pt-2 pb-3 space-y-1">
-          <Link href="/" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+          <Link href="/about" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
             Nosotros
           </Link>
           {user ? (
